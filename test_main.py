e7135d18c03f59aea5471a25fe0f4e82742853f4
mbaumann1 <mbaumann1@luc.edu>
2015-09-14 18:39:10 -0500

    Added simple tests for whether the csv files exist and can be accessed.

---
+++ test_main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# test_main.py authored by Matt Baumann for COMP 412 Loyola University
+
+import main
+import unittest
+import os
+#import os.path
+
+class MainTests(unittest.TestCase):
+    
+    ##http://stackoverflow.com/questions/82831/check-whether-a-file-exists-using-python
+    def testContractorsIsFile(self):
+        assert os.path.isfile('./contractors.csv') and os.access('./contractors.csv', os.R_OK)
+    def testLobbyistsIsFile(self):
+        assert os.path.isfile('./lobbyist.csv') and os.access('./lobbyist.csv', os.R_OK)
+    
+if __name__ == "__main__": unittest.main()
